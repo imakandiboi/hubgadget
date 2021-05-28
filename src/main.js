781dd2458fea1f2779f4aaa8fff343030c552348
chiagozie Arinzechi <imakandiboi@yahoo.com>
2021-05-28 15:45:57 +0100

    commit message

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import VueAgile from "vue-agile";
+
+Vue.use(VueAgile);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        explorerGreen: {
+          "100": "#72e86f",
+          "200": "#35a84f",
+          "300": "#3fb93a",
+          "400": "#3b9b1e",
+          "500": "#2c6407",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
